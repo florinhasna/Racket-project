56933f51f13abaae4df2493ad781ba24c158f10a
florinhasna <123017602+florinhasna@users.noreply.github.com>
2023-01-30 11:44:15 +0000

    Update Project.rkt
    I have created the functions to generate the route, created the graphs, made the GUI buttons work, added "Invert" button to invert the selected stations.

--- Project.rkt
+++ Project.rkt
@@ -8,21 +8,21 @@
                    [parent block2]
                    [alignment '(center center)]))
 
-(struct metline (station accessibility toilets shops))
-(define npark (metline "Northwick Park" "(R)" #f #f))
-(define kent-met (metline "Kenton" "(R)" #f #f))
-(define baker (metline "Baker Street" "(A)" #t #t))
-(define euston (metline "Euston Square" "(R)" #f #f))
-(define wemcen-met (metline "Wembley Central" #f #t #f))
-(define liverpool (metline "Liverpool Street" #f #f #t))
-(define wempark (metline "Wembley Park" "(A)" #t #t))
+(struct metline (station))
+(define npark (metline "Northwick Park (R)"))
+(define kent-met (metline "Kenton (R)"))
+(define baker (metline "Baker Street (A) (T)"))
+(define euston (metline "Euston Square (R)"))
+(define wemcen-met (metline "Wembley Central (T)"))
+(define liverpool (metline "Liverpool Street"))
+(define wempark (metline "Wembley Park (A) (T)"))
 
-(struct bkrline (station accessibility toilets shops))
-(define harrow (bkrline "Harrow" "(A)" #t #f))
-(define kent-bkr (bkrline "Kenton" "(R)" #f #f))
-(define sken (bkrline "South Kenton" "(A)" #f #f))
-(define nwem (bkrline "North Wembley" "R" #f #f))
-(define wemcen-bkr (bkrline "Wembley Central" #f #t #f))
+(struct bkrline (station))
+(define harrow (bkrline "Harrow (A) (T)"))
+(define kent-bkr (bkrline "Kenton (R)"))
+(define sken (bkrline "South Kenton (A)"))
+(define nwem (bkrline "North Wembley (R)"))
+(define wemcen-bkr (bkrline "Wembley Central (T)"))
 
 (define stations (list "Select"
                         (bkrline-station harrow)
@@ -35,80 +35,122 @@
                         (metline-station euston)
                         (metline-station liverpool)
                         (metline-station wempark)))
-
-(define links (list (list (bkrline-station harrow) (bkrline-station kent-bkr))
+                        
+(define bkr-graph (list (list (bkrline-station harrow) (bkrline-station kent-bkr))
                     (list (bkrline-station kent-bkr) (bkrline-station sken))
-                    (list (bkrline-station sken) (bkrline-station nwem))
-                    (list (bkrline-station nwem) (bkrline-station wemcen-bkr))
-                    (list (bkrline-station wemcen-bkr) (bkrline-station nwem))
-                    (list (bkrline-station nwem) (bkrline-station sken))
-                    (list (bkrline-station sken) (bkrline-station kent-bkr))
-                    (list (bkrline-station kent-bkr) (bkrline-station harrow))
-                    (list (metline-station npark) (metline-station kent-met))
-                    (list (metline-station kent-met) (metline-station baker))
-                    (list (metline-station baker) (metline-station euston))
-                    (list (metline-station euston) (metline-station wemcen-met))
-                    (list (metline-station wemcen-met) (metline-station liverpool))
-                    (list (metline-station liverpool) (metline-station wempark))
-                    (list (metline-station wempark) (metline-station npark))
-                    (list (metline-station kent-met) (metline-station npark))
-                    (list (metline-station npark) (metline-station wempark))
-                    (list (metline-station wempark) (metline-station liverpool))
-                    (list (metline-station liverpool) (metline-station wemcen-met))
-                    (list (metline-station wemcen-met) (metline-station euston))
-                    (list (metline-station euston) (metline-station baker))
-                    (list (metline-station baker) (metline-station kent-met))
-                    (list (metline-station kent-met) (metline-station npark))
-                    ))
+                    (list (bkrline-station sken) (bkrline-station nwem)) 
+                    (list (bkrline-station nwem) (bkrline-station wemcen-bkr))))
 
-(define route '())
+(define met-graph (list (list (metline-station npark) (bkrline-station kent-bkr))
+                    (list (bkrline-station kent-bkr) (metline-station baker))
+                    (list (metline-station baker) (metline-station euston))
+                    (list (metline-station euston) (bkrline-station wemcen-bkr))
+                    (list (bkrline-station wemcen-bkr) (metline-station liverpool))
+                    (list (metline-station liverpool) (metline-station wempark))
+                    (list (metline-station wempark) (metline-station npark))))
+
+(define empty-list '())
+
+(define reverse-line (lambda (a-graph empty-lst)
+                      (cond
+                        ((empty? a-graph) empty-lst)
+                        (#t (reverse-line (rest a-graph) (append (cons (reverse (first a-graph)) '()) empty-lst))))))
+
+(define bakerloo (append bkr-graph (reverse (reverse-line bkr-graph empty-list))))
+(define metropolitan (append met-graph (reverse (reverse-line met-graph empty-list))))
+(define forward-backward (append bkr-graph met-graph (reverse (reverse-line bkr-graph empty-list)) (reverse (reverse-line met-graph empty-list))))
 
 (define edge (lambda (x graph)
                (map (lambda (y)(first (rest y))) (filter (lambda (y) 
                                                            (eq? (first y) x)) graph))))
 
-(define path (lambda (x y route graph vertex-set) 
+(define path-check (lambda (x y graph vertex-set) 
                (cond
-                 ((eq? x y) (reverse (cons x route)))
-                 ((not (set-member? vertex-set x)) "Something is wrong")
-                 ((not (set-member? vertex-set y)) "Something is wrong")
-                 (#t (ormap (lambda (z) (path z y (cons x route) graph (set-remove vertex-set x))) (edge x graph)))
-                 )
+                 ((eq? x y) #t)
+                 ((not (set-member? vertex-set x)) #f)
+                 ((not (set-member? vertex-set y)) #f)
+                 (#t (ormap (lambda (z) (path-check z y graph (set-remove vertex-set x))) (edge x graph)))
+               )
                )
   )
 
-(define plan (lambda (x y) ;; (plan "Harrow" "Liverpool Street")
-                    (path x y route links (list->set (flatten links)))))
+(define create-route (lambda (from to route graph vertex-set) 
+                       (cond 
+                         ((eq? from to) (reverse (cons from route)))
+                         ((not (set-member? vertex-set from)) #f) 
+                         ((not (set-member? vertex-set to)) #f)
+                         (#t (ormap (lambda (z) (create-route z to (cons from route) graph (set-remove vertex-set from))) (edge from graph)))
+                         )
+                       )
+  )
 
-;(define c1 (new choice%
-                 ;   [label "Location: "]
-                 ;   [choices stations]
-                 ;   [parent panel]))
+(define reachable (lambda (from to line) (path-check from to line (list->set (flatten line)))))
+(define plan1 (lambda (from to line) (create-route from to empty-list line (list->set (flatten line)))))
 
-;(define c2 (new choice%
-            ;         [label "Destination: "]
-                  ;   [choices stations]
-                  ;   [parent panel]))
+(define plan (lambda (from to)  
+                 (cond      
+                   ((reachable from to bakerloo) (plan1 from to bakerloo))
+                   ((reachable from to metropolitan) 
+                    (cond
+                      ((< (length (plan1 from to metropolitan)) (length (plan1 from to (reverse metropolitan)))) (plan1 from to metropolitan))
+                      (#t (plan1 from to (reverse metropolitan)))
+                      ))
+                   ((reachable from to forward-backward)
+                    (cond
+                      ((= (length (plan1 from to forward-backward)) (length (plan1 from to (reverse forward-backward))))
+                       (append (plan1 from to forward-backward) (plan1 from to (reverse forward-backward))))
+                      ((< (length (plan1 from to forward-backward)) (length (plan1 from to (reverse forward-backward)))) (plan1 from to forward-backward)) 
+                      (#t (plan1 from to (reverse forward-backward)))))
+                   (#t "Please select your leaving station and destination!")
+                   )
+               )
+  )
 
-(define from (new text-field%
-                        [label "From: "] [parent block2]))
+(define plan-string (lambda (from to)
+                      (for/list ([i (plan from to)])
+                        (cond
+                          ((eq? i (last (plan from to))) i)
+                          (#t (string-append i " -> "))
+                          )
+                        )
+                      )
+  )
 
-(define to (new text-field%
-                        [label "To:     "] [parent block2]))
+(define from (new choice%
+                    [label "Location: "]
+                    [choices stations]
+                    [parent panel]))
+
+(define to (new choice%
+                     [label "Destination: "]
+                     [choices stations]
+                     [parent panel]))
+
+(define invert-stations (new button%
+                             [label "Invert"]
+                             [parent block2]
+                             [callback (lambda (o e)
+                                         (let ([i (send from get-selection)])
+                                         (send from set-selection (send to get-selection))
+                                         (send to set-selection i)))]))
 
 (define faster (new check-box%
                        [label "Fastest route"] [parent block2]                      
                        [value #t]
                        )) ;if value #f the box is not ticked
 
-(define button (new button%
+(define get-me-there (new button%
                     [parent block2]
                     [label "Get me there!"]
                     [callback (lambda (o e)
-                (send text-field set-value (plan (send from get-value) (send to get-value))))]))
+                                (send route-plan set-value
+                                      (cond
+                                        ((string? (plan (list-ref stations (send from get-selection)) (list-ref stations (send to get-selection)))) (plan (list-ref stations (send from get-selection)) (list-ref stations (send to get-selection))))
+                                        (#t (apply string-append (plan-string (list-ref stations (send from get-selection)) (list-ref stations (send to get-selection))))))))]))
 
-(define text-field (new text-field%
-                        [label "Route plan: "] [parent block2]))
+(define route-plan (new text-field%
+                        [label "Route plan: "] [parent block2]
+                        ))
 
 
 
